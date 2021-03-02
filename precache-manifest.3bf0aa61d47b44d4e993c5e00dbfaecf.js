b99a27d08af7fbe4f4e05d10eda3191696c1b3ec
Alex Tsoi <alex@k2.digital>
2021-03-02 13:09:48 +0800

    updated fb:

---
+++ precache-manifest.3bf0aa61d47b44d4e993c5e00dbfaecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d62739dffe3fd2117f6aceeec923c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcbf5349efe3a7024d2d",
+    "url": "/static/css/main.a130a2cb.chunk.css"
+  },
+  {
+    "revision": "d30f70a4c7ecd9c9c751",
+    "url": "/static/js/2.ecf39b47.chunk.js"
+  },
+  {
+    "revision": "adf988142ef3edd1ada16f48aab2de8c",
+    "url": "/static/js/2.ecf39b47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcbf5349efe3a7024d2d",
+    "url": "/static/js/main.66727c70.chunk.js"
+  },
+  {
+    "revision": "d13e4bc4c7e8b0344ab1",
+    "url": "/static/js/runtime-main.a24f6be9.js"
+  }
+]);
